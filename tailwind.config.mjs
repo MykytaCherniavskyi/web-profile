40e2b07386d4dfaa3048b41eb525390775b3a129
nchernyavsky <mikita1234qwer@gmail.com>
2024-11-30 10:22:21 +0100

    removed fluid package

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,15 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
-import fluid, { extract, fontSize } from 'fluid-tailwind'
 import daisyui from "daisyui";
 
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: {
 		files: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-		extract,
 	},
 	daisyui: {
 		themes: ["cupcake"],
 	},
 	theme: {
-		fontSize,
 		extend: {
 			fontFamily: {
 				sans: ['Roboto', ...defaultTheme.fontFamily.sans],
@@ -20,5 +17,5 @@ export default {
 			}
 		},
 	},
-	plugins: [daisyui, fluid],
+	plugins: [daisyui],
 }
